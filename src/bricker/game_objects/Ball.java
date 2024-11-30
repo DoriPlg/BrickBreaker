fa27beef0a41eba79d17bb67512c9ffc9357c443
DoriPlg <1dor.peleg@gmail.com>
2024-11-30 11:01:14 +0200

    ball basic

--- src/bricker/game_objects/Ball.java
+++ src/bricker/game_objects/Ball.java
@@ -1,7 +1,26 @@
 package src.bricker.game_objects;
 
 import danogl.GameObject;
+import danogl.collisions.Collision;
+import danogl.gui.ImageReader;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
 
 public class Ball extends GameObject {
+    private int collisions;
+    public Ball(int x, int y, ImageReader imageReader) {
+        super(new Vector2(x,y), new Vector2(50, 50), null);
+        Renderable ballImage =
+                imageReader.readImage("assets/assets/ball.png", true);
+        this.collisions = 0;
+    }
+
+    public void onCollisionEnter(GameObject otherObject, Collision collision) {
+        this.collisions++;
+    }
+
+    public int getCollisionCounter(){
+        return this.collisions;
+    }
 
 }
--- src/bricker/game_objects/Paddle.java
+++ src/bricker/game_objects/Paddle.java
@@ -1,4 +1,9 @@
 package src.bricker.game_objects;
 
 public class Paddle {
+    private static final int PADDLE_WIDTH = 20;
+    private static final int PADDLE_HEIGHT = 5;
+    public Paddle() {
+        
+    }
 }
