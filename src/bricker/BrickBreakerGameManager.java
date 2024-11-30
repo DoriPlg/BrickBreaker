51fafde426d62640353f86e324ce3d88c60073d4
DoriPlg <1dor.peleg@gmail.com>
2024-11-30 10:24:02 +0200

    starting structure

---
+++ src/bricker/BrickBreakerGameManager.java
@@ -0,0 +1,11 @@
+package src.bricker;
+
+import danogl.GameManager;
+import danogl.util.Vector2;
+
+public class BrickBreakerGameManager {
+
+    public static void main(String[] args) {
+
+    }
+}
