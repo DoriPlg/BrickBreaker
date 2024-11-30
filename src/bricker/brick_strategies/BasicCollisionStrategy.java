233e51309eb7342eb99d6865b6b49c977205f599
DoriPlg <1dor.peleg@gmail.com>
2024-11-30 17:05:04 +0200

    lifeCount with all hearts, subtraction via API

--- src/bricker/brick_strategies/BasicCollisionStrategy.java
+++ src/bricker/brick_strategies/BasicCollisionStrategy.java
@@ -1,20 +1,10 @@
 package src.bricker.brick_strategies;
 
 import danogl.GameObject;
-import src.bricker.game_objects.Ball;
-import src.bricker.game_objects.Brick;
 
 public class BasicCollisionStrategy implements CollisionStrategy {
-
-    private static final float BALL_ACCELERATION_ON_HIT = 1.06f;
+    public BasicCollisionStrategy() {}
 
     @Override
-    public void onCollision(GameObject thisObj, GameObject otherObj) {
-        if (thisObj instanceof Brick) {
-            if (otherObj instanceof Ball) {
-                otherObj.setVelocity(otherObj.getVelocity().mult(BALL_ACCELERATION_ON_HIT));
-            }
-        }
-
-    }
+    public void onCollision(GameObject thisObj, GameObject otherObj) {}
 }
--- src/bricker/game_objects/LifeCount.java
+++ src/bricker/game_objects/LifeCount.java
@@ -7,13 +7,27 @@ import java.awt.*;
 
 public class LifeCount extends GameObject {
     private static final Vector2 HEART_SIZE = new Vector2(20, 20);
-
     private int lives;
 
     public LifeCount(Vector2 position, Renderable renderable, int lives) {
         super(position, HEART_SIZE, renderable);
         this.lives = lives;
     }
+
+    @Override
+    public void render(Graphics2D g)
+    /*
+    * Renders the life count on the screen,
+    * by drawing the heart image multiple times
+    */
+   {
+        for (int i = 0; i < lives; i++) {
+            renderer().getRenderable().render(g,
+                    new Vector2(getTopLeftCorner().x() + i * (int) HEART_SIZE.x(), getTopLeftCorner().y()),
+                    getDimensions()
+             );
+        }
+   }
 
     public void loseLife() {
         lives--;
