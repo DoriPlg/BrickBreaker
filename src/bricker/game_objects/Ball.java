42c886726b72b6cba6f1d34e05f43d87c5e3d5df
dori.plg <dori.peleg@mail.huji.ac.il>
2024-12-01 17:27:23 +0200

    a constant SIZE for Ball

--- src/bricker/game_objects/Ball.java
+++ src/bricker/game_objects/Ball.java
@@ -13,10 +13,11 @@ public class Ball extends GameObject {
     private static final float BALL_SPEED = 150;
     private final Sound collissionSound;
     private int collisions;
+    private static final Vector2 STANDARD_BALL_SIZE = new Vector2(50, 50);
 
 
     public Ball(Vector2 startLoc, Renderable renderable, Sound collissionSound) {
-        super(startLoc, new Vector2(50, 50), renderable);
+        super(startLoc, STANDARD_BALL_SIZE, renderable);
         this.collissionSound = collissionSound;
         float ballVelX = BALL_SPEED;
         float ballVelY = BALL_SPEED;
