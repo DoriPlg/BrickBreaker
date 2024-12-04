fb1fc90ce458d6b32d155d1e25d8b8603c120fc5
DoriPlg <1dor.peleg@gmail.com>
2024-12-04 10:06:05 +0200

    all collision strategies extend the basic one!

--- src/bricker/brick_strategies/TurboCollisionStrategy.java
+++ src/bricker/brick_strategies/TurboCollisionStrategy.java
@@ -1,15 +1,19 @@
 package src.bricker.brick_strategies;
 
 import danogl.GameObject;
+import src.bricker.BrickBreakerGameManager;
 import src.bricker.game_objects.Ball;
 import src.bricker.game_objects.Puck;
 
-public class TurboCollisionStrategy implements CollisionStrategy {
+public class TurboCollisionStrategy extends BasicCollisionStrategy implements CollisionStrategy {
 
-    public TurboCollisionStrategy() {}
+    public TurboCollisionStrategy(BrickBreakerGameManager gameManager) {
+        super(gameManager);
+    }
 
     @Override
     public void onCollision(GameObject thisObj, GameObject otherObj) {
+        super.onCollision(thisObj, otherObj);
         if (!(otherObj instanceof Puck) && otherObj instanceof Ball ball){
             ball.turboModeOn();
         }
